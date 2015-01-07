7bb71c18c0dd3d27b7f5b0f6987b97cd24a9d864
Simone Murzilli <simone.murzilli@fao.org>
2015-01-07 16:17:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(
-    name='GeobricksFTPManager',
+    name='GeobricksStorageManager',
     version='0.0.1',
     author='Simone Murzilli; Guido Barbaglia',
     author_email='geobrickspy@gmail.com',
     packages=find_packages(),
     license='LICENSE.txt',
     long_description=open('README.md').read(),
-    description='Geobricks FTP manager.',
+    description='Geobricks storage manager.',
     install_requires=[
         'GeobricksCommon',
     ],
-    url='http://pypi.python.org/pypi/GeobricksFTPManager/',
+    url='http://pypi.python.org/pypi/GeobricksStorageManager/',
     keywords=['geobricks', 'metadata', 'geoserver', 'd3s']
 )
